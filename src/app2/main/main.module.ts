2af9693001053ff1e2c04570d12c774c81c5c5af
nclsbayona <nbayonaord@gmail.com>
2022-01-31 13:20:16 -0500

    added app 2

---
+++ src/app2/main/main.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MainComponent } from './main.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [],
+  bootstrap: [MainComponent],
+})
+export class MainModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { MainModule } from './app2/main/main.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+platformBrowserDynamic()
+  //.bootstrapModule(AppModule)
+  .bootstrapModule(MainModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
